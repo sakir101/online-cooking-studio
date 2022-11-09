d7547f04ed4567feb27ae158beda0208eeb79a3c
sakir101 <sakirhossainf99@gmail.com>
2022-11-09 19:51:10 +0600

    review section

--- src/Home/Services/SingleServices.js
+++ src/Home/Services/SingleServices.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Reviews from '../../Pages/Reviews/Reviews';
 
 const SingleServices = () => {
+    const {title, img, desc, price, rating} = useLoaderData();
+   
     return (
-        <div>
-            
+        <div className='p-5'>
+           <div className='text-center'>
+                <h3 className='text-5xl font-bold my-5'>{title}</h3>
+                <h4 className='text-2xl text-blue-700 my-3'>Price: {price} BDT</h4>
+                <h4 className='text-2xl text-blue-700 my-3'>Rating: {rating}/5</h4>
+                <img src={img} className='w-1/2 mx-auto my-3' alt="" />
+            </div> 
+            <p className='my-5'>{desc}</p>
+            <Reviews></Reviews>
         </div>
     );
 };
---
+++ src/Pages/Reviews/Reviews.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div className='flex justify-between my-6'>
+            <h3 className='text-2xl font-bold'>People's Review</h3>
+            <button className='btn btn-outline'>Add Review</button>
+        </div>
+    );
+};
+
+export default Reviews;
