7a59400958c1369623ed5d0aa0bbaf426d3aca07
sakir101 <sakirhossainf99@gmail.com>
2022-11-09 15:28:33 +0600

    all services added

--- src/Home/Services/AllService.js
+++ src/Home/Services/AllService.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import AllServicesCard from './AllServicesCard';
 
 const AllService = () => {
+    const [allservices, setAllServices] = useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/allservices')
+        .then(res => res.json())
+        .then(data => setAllServices(data))
+    },[])
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-6 my-6 w-3/4 mx-auto'>
+            {
+                allservices.map(allservice => <AllServicesCard key={allservice._id} allservice={allservice}></AllServicesCard>)
+            }
         </div>
     );
 };
